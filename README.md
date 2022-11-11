eda2928244b4175c3e4db20b0622b2430c47db90
WellshCorgi <gillco00@naver.com>
2022-11-11 15:30:57 +0900

    update 2022/11/11

--- README.md
+++ README.md
@@ -4,16 +4,20 @@ To notice about Hongik University's cafeteria menu at every 10:30am [file of hon
 path on Discord app [discordbot.py], Developed by python.
 
 ## Prerequisite
-Used pakage -> requests, BeautifulSoup4, schedule, datatime, time ,Bot
+Used pakage -> requests, BeautifulSoup4, schedule, datatime, time ,Bot , discord.py, html
 
 ## Files
-'Hongik_notice_Cafeteria.py' is written in Python and is a working file.
+'Hongik_notice_Cafeteria.py' is written by Python and is a working file./n
+'discordbot.py' is written by Python and working file on Discord but you may make bot application Frist.
 
-### Updates
+### Updates verion0.1
 2022/11/08 -> When executing code, I can check an errors. Completeing partial of Function [cycle]
 
-## Release
+### Updates version 0.2
 2022/11/10 -> pathed on Discord by Discord Bot, if you are hongik univ student you can use this code 'just type your bot token'
+
+## Release
+2022/11/11 -> removed some function and some function was slightly modified, [The function to receive an alarm at a specific time will be implemented soon]
 
 
 Thanks for the advice --J-hoplin / https://github.com/J-hoplin1
--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime,date
 
 
-TOKEN = 'your discode bot token'
+TOKEN = 'put your discord-bot Token'
 intents = discord.Intents.default()
 bot = Bot(command_prefix='!', intents=intents)
 @bot.event
@@ -21,31 +21,29 @@ async def menu(message):
 
   menu_lunch_data=[html.select(".foodmenu")[i].get_text() for i in range(0,5)] # Put in menu_lunch_data in html-> 'class==foodmenu'
   menu_dinner_data=[html.select(".foodmenu")[i].get_text() for i in range(6,11)] # same dinner data
-  for i in range(0,5):
-    day_menu.append(menu_lunch_data[i].strip()+"\n\n"+menu_dinner_data[i].strip()) # Remove data's space
+  day_menu=[menu_lunch_data[i].strip()+"\n\n"+menu_dinner_data[i].strip() for i in range(0,5)] # Remove data's space
+  menu_dinner_data.append("주말에는 B동 학교식당을 운영하지 않습니다.") # Add exception ->if>>  Cafeteria open day
+  menu_lunch_data.append("주말에는 B동 학교식당을 운영하지 않습니다.")
 
   days=["mon","tue","wed","thr","fri"]
   dictionary = {key:value for key,value in zip(days,day_menu)} # Using by Comprension -> create Dictionary
-
+  
+  if not dictionary['mon']: # This If sentense that prints an error message when there is no data in the site
+    menu_lunch_data.append("학교 공식 사이트에 메뉴 정보가 없습니다. 업로드를 기다려주세요")
+    
   def cycle(date): # A function that retrieves a date and finds a dictionary value
       days=["mon","tue","wed","thr","fri"] 
       day = date.weekday()
       print(days[day] if day == "sat" or "sun" else "주말에는 B동 학식을 운영하지 않습니다")
-      return day # amend part -> version upgrade
-    
-  def find_menu(): # A function that prints an error message when there is no data in the site
-      if not dictionary['mon']:
-           print("학교 공식 사이트에 메뉴 정보가 없습니다. 업로드를 기다려주세요")
-      else:
-          print(dictionary[cycle(date.today())])
+      return day # amend part -> version upgrade   
             
-  embed = discord.Embed(title="B동(세움관) 학교식당 중식 메뉴입니다.",description="",color=0xff0000)
-  embed.set_author(name="홍익대학교 정보 알리미", url="https://sj.hongik.ac.kr/site/food/food_menu.html", icon_url="https://www.hongik.ac.kr/front/images/local/header_logo.png")
+  embed = discord.Embed(title="B동(세움관) 학교식당 중식 메뉴입니다.",description="",color=0x0aa40f)
+  embed.set_author(name="홍익대학교 정보 알리미", url="https://sj.hongik.ac.kr/site/food/food_menu.html", icon_url="https://www.hongik.ac.kr/front/images/local/pic_ui_01_01.png")
   embed.add_field(name="오늘의 중식", value=menu_lunch_data[cycle(date.today())] , inline=True)
   embed.add_field(name="오늘의 석식", value=menu_dinner_data[cycle(date.today())] , inline=True)
   embed.set_thumbnail(url="https://www.hongik.ac.kr/front/images/local/header_logo.png")
-  embed.set_footer(text="자세한 정보는 위의 프로필 버튼의 사이트를 방문해주세요.", icon_url="https://cdn.discordapp.com/attachments/779326874026377226/789098688315654164/Flag_of_South_Korea.png")
-
+  embed.set_footer(text="Developed by WellshCorgi.", icon_url="https://cdn.discordapp.com/attachments/779326874026377226/789098688315654164/Flag_of_South_Korea.png")
+  
   await message.send(embed=embed)
 
 bot.run(TOKEN)
